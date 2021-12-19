7206fb40b577c2290b4b0f532890c30ec42725f7
joyce <joyce44528@gmail.com>
2021-12-20 00:07:38 +0800

    第一版產品卡片元件完成

---
+++ compass/app/js/screens/productScreen.js
@@ -0,0 +1,33 @@
+import data from '../data.js';
+
+const ProductScreen = {
+    render: () => {
+        const {products} = data;
+
+        return `
+        <ul class="products col-3-of-4">
+            ${products.map(
+                (product) => `
+                <li class="product-card">
+                    <a href="/#/product/${product._id}" class="product-img">
+                        <img src="${product.image}" alt="${product.name}">
+                        <button class="btn add-cart">
+                            <img src="imgs/SVG/cart.svg" alt="shop cart icon">
+                        </button>
+                    </a>
+                    <div class="card-txt">
+                        <a href="##" class="card-tag">${product.category}</a>
+                        <a class="card-title">${product.name}</a>
+                        <div class="card-detail">
+                            <a class="card-color"></a>
+                            <div class="card-price">NT $${product.price}</div>
+                        </div>
+                    </div>
+                </li>
+                `
+            )}
+                
+            </ul>
+        `
+    }
+}
