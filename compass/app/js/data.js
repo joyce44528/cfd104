5a2feb227d7ce06046c7c76844b634a2a60d4fd8
joyce <joyce44528@gmail.com>
2021-12-19 23:52:35 +0800

    新增到5筆資料，要做渲染測試中

--- compass/app/js/data.js
+++ compass/app/js/data.js
@@ -19,6 +19,39 @@ export default {
             color: ['#fff','#BFA686'],
             numReviews: 21,
             countInStock: 6
+        },
+        {
+            _id: '3',
+            name: '秋穫',
+            category: ['桌花','大型桌花'],
+            image: '../imgs/product-3-square.jpeg',
+            price: '1200',
+            color: ['#fff','#BFA686'],
+            numReviews: 8,
+            countInStock: 4
+        },
+        {
+            _id: '4',
+            name: '浪漫粉裸花束',
+            category: ['花束','裸花束'],
+            image: '../imgs/product-4-square.jpeg',
+            price: '420',
+            color: ['#fff','#CBAAA0'],
+            numReviews: 30,
+            countInStock: 11
+        },
+        {
+            _id: '5',
+            name: '白色系小桌花組',
+            category: ['桌花','小型桌花'],
+            image: '../imgs/product-5-square.jpeg',
+            price: '1000',
+            color: ['#fff','#B69961'],
+            numReviews: 25,
+            countInStock: 7
         }
+
+
+
     ]
 }
