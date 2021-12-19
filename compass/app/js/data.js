e88f5fc9e10cbd82fcd7dae98f4d2e8b186f8273
joyce <joyce44528@gmail.com>
2021-12-19 23:33:50 +0800

    新增data.js / 兩組產品資料

---
+++ compass/app/js/data.js
@@ -0,0 +1,24 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: '純白飛鳥草',
+            category: ['桌花','小型桌花'],
+            image: '../imgs/product-1-square.jpeg',
+            price: '200',
+            color: ['#fff'],
+            numReviews: 10,
+            countInStock: 5
+        },
+        {
+            _id: '2',
+            name: '白白綿綿',
+            category: ['桌花','中型桌花'],
+            image: '../imgs/product-2-square.jpeg',
+            price: '550',
+            color: ['#fff','#BFA686'],
+            numReviews: 21,
+            countInStock: 6
+        }
+    ]
+}
